78b22bcc028945cf1ca33d0462317b13339f42e1
Taylor Bauer <taylorbauer@ku.edu>
2019-02-04 15:57:43 -0600

    Added functioning search(T value) member variable.

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -59,9 +59,18 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	while (temp != nullptr)
+	{
+		if (temp->getValue() == value)
+		{
+			return true;
+		}
+		else
+		{
+			temp = temp->getNext();
+		}
+		
+	}
 
 	return(isFound);
 }
